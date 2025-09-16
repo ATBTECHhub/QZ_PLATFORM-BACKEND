807e911ba6a9330e65c501e29a829645476e7a14
Dalaktronixs <hakeemabdullah87@gmail.com>
2025-09-16 13:01:12 +0100

    BUGFIX: database configuration for production

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,9 +4,8 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      ssl: false, // Enable SSL
-      tls: false, // Enable TLS
-      tlsAllowInvalidCertificates: false, // Allow invalid certificates (use with caution in development)
+      //ssl: true, // Enable SSL
+      //tls: true, // Enable TLS
       serverSelectionTimeoutMS: 5000, // 5 seconds timeout
     });
     console.log('MongoDB connected');
